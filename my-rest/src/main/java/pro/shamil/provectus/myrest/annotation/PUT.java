25208f88eea95f24b064d48b8a33b5742f816bbd
Shamil Sungatov <shamil.sungatov@gmail.com>
2017-02-13 22:45:57 +0300

    Added additional annotations

---
+++ my-rest/src/main/java/pro/shamil/provectus/myrest/annotation/PUT.java
@@ -0,0 +1,7 @@
+package pro.shamil.provectus.myrest.annotation;
+
+/**
+ * Created by regal on 13.02.17.
+ */
+public @interface PUT {
+}
---
+++ my-rest/src/main/java/pro/shamil/provectus/myrest/proxy/RestResourceProxy.java
@@ -0,0 +1,7 @@
+package pro.shamil.provectus.myrest.proxy;
+
+/**
+ * Created by regal on 13.02.17.
+ */
+public class RestResourceProxy {
+}
